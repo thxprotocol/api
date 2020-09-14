e06f16e15fc9bcf6482f2926bb5f3611420fc235
Peter Polman <peter@peterpolman.nl>
2020-09-14 13:11:37 +0200

    [THX-255] Adds first working test for unauthorized access of /account

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -64,19 +64,5 @@ export const isAuthenticated = (req: Request, res: Response, next: NextFunction)
     if (req.isAuthenticated()) {
         return next();
     }
-    res.redirect('/login');
-};
-
-/**
- * Authorization Required middleware.
- */
-export const isAuthorized = (req: Request, res: Response, next: NextFunction) => {
-    const provider = req.path.split('/').slice(-1)[0];
-
-    const account = req.user as AccountDocument;
-    if (_.find(account.tokens, { kind: provider })) {
-        next();
-    } else {
-        res.redirect(`/auth/${provider}`);
-    }
+    res.status(401).end();
 };
--- test/api.test.ts
+++ test/api.test.ts
@@ -1,8 +1,8 @@
 import request from 'supertest';
 import app from '../src/app';
 
-describe('GET /api', () => {
-    it('should return 200 OK', (done) => {
-        request(app).get('/').expect(200);
+describe('GET /account', () => {
+    it('should return 401 when not authenticated', (done) => {
+        request(app).get('/v1/account').expect(401, done);
     });
 });
