65bff5bd387a43268863d63341b973b17143f9fd
Peter Polman <peter@peterpolman.nl>
2020-10-15 21:23:37 +0200

    Fixes local tests for CICD

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -1,19 +1,18 @@
 import dotenv from 'dotenv';
 import logger from './logger';
 
-dotenv.config();
+dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.example' : '.env' });
 
 export const VERSION = 'v1';
 export const ENVIRONMENT = process.env.NODE_ENV;
-export const PRIVATE_KEY = process.env['PRIVATE_KEY'];
-export const ADDRESS = process.env['ADDRESS'];
 export const MONGODB_URI = process.env['MONGODB_URI'];
+export const PRIVATE_KEY = process.env['PRIVATE_KEY'];
 export const SESSION_SECRET = process.env['SESSION_SECRET'];
 export const APP_URL = process.env['APP_URL'];
+export const RPC = process.env['RPC'];
 
 if (!MONGODB_URI) {
     logger.error('No mongo connection string. Set MONGODB_URI environment variable.');
-
     process.exit(1);
 }
 
