c4acd81456c1d7b63baa6c0ecffdf3e10d1f34bb
Peter Polman <peter@peterpolman.nl>
2020-11-03 10:11:21 +0100

    Updates contracts and fixes issue in pretest script

--- scripts/deployGasStation.js
+++ scripts/deployGasStation.js
@@ -1,10 +1,10 @@
+const Web3 = require('web3');
 const dotenv = require('dotenv');
 const GAS_STATION = require('../src/artifacts/GasStation.json');
 
 dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
 
 (async () => {
-    const Web3 = require('Web3');
     const web3 = new Web3(process.env.RPC);
     const admin = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
     const gasStationContract = new web3.eth.Contract(GAS_STATION.abi);
