0c0642392efec54a6c6a3bf5c2c8d23bd9268dee
Peter Polman <peter@peterpolman.nl>
2021-01-11 14:21:58 +0100

    Fixes broken paths in ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
     apps: [
         {
             name: 'api',
-            script: '$HOME/api/dist/src/server.js',
+            script: 'dist/src/server.js',
             interpreter: 'none',
             env: {
                 NODE_ENV: 'development',
             },
-            error_file: '$HOME/api/logs/error.log',
-            out_file: '$HOME/api/logs/error.log',
+            error_file: 'logs/error.log',
+            out_file: 'logs/error.log',
             log_date_format: 'YYYY-MM-DD HH:mm Z',
         },
     ],
