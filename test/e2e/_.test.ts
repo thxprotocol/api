16ed984818a67a6c38c46f7c49d9c791af1ddc6b
Evert Kors <evert@hatching.io>
2021-01-18 18:18:06 +0100

    All tests are working

--- test/e2e/_.test.ts
+++ test/e2e/_.test.ts
@@ -27,7 +27,7 @@ afterAll(async () => {
 });
 
 require('./api.ts');
-// require('./auth.ts');
-// require('./roles.ts');
-// require('./encrypt.ts');
-// require('./gasStation.ts');
+require('./auth.ts');
+require('./roles.ts');
+require('./encrypt.ts');
+require('./gasStation.ts');
