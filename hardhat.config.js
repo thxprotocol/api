ecddfc56eb3f6770e55581c0a2a8fb19da955dd0
Peter Polman <peter@peterpolman.nl>
2021-01-18 11:20:43 +0100

    Fixes hardhat config for CI

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,20 @@
 require('dotenv').config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
 require('@nomiclabs/hardhat-ethers');
 
+const pkey = process.env.PRIVATE_KEY || '0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709';
+const url = process.env.PUBLIC_RPC || 'http://localhost:8545';
+
 module.exports = {
     networks: {
         default: {
-            url: process.env.PUBLIC_RPC,
-            accounts: [process.env.PRIVATE_KEY],
+            url,
+            accounts: [pkey],
             gas: 6e6,
         },
         hardhat: {
             accounts: [
                 {
-                    privateKey: process.env.PRIVATE_KEY,
+                    privateKey: pkey,
                     balance: '1000000000000000000000000000000000000000',
                 },
             ],
