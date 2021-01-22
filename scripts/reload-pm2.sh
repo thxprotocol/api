63eca92f71490098369d86af6fb79af60e10507d
Peter Polman <peter@peterpolman.nl>
2021-01-22 13:52:21 +0100

    Fixes elif statement in shell script

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ "$DEPLOYMENT_GROUP_NAME" == "develop" ]; then
-    pm2 kill && pm2 start ecosystem.config.js --env staging --update-env 
+  pm2 kill && pm2 start ecosystem.config.js --env staging --update-env
 elif [ "$DEPLOYMENT_GROUP_NAME" == "master" ]; then
-    pm2 kill && pm2 start ecosystem.config.js --env production --update-env 
+  pm2 kill && pm2 start ecosystem.config.js --env production --update-env
 else
-  # Fail the deployment
+  echo "WARNING: Deployment failed.";
 fi
