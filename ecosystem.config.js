49926a41170fa6f3d5b2fe17a9001ca85145d2fc
Peter Polman <peter@peterpolman.nl>
2021-01-22 13:35:31 +0100

    Introduces PM2 loadbalancer and makes reload script environment specific

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,12 @@ module.exports = {
         {
             name: 'api',
             script: 'dist/src/server.js',
-            env: {
+            instances: 'max',
+            env_staging: {
                 NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'production',
             },
             error_file: 'logs/error.log',
             out_file: 'logs/combined.log',
--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-pm2 kill && pm2 start ecosystem.config.js --update-env
+if [ "$DEPLOYMENT_GROUP_NAME" == "develop" ]; then
+    pm2 kill && pm2 start ecosystem.config.js --env staging --update-env 
+elif [ "$DEPLOYMENT_GROUP_NAME" == "master" ]; then
+    pm2 kill && pm2 start ecosystem.config.js --env production --update-env 
+else
+  # Fail the deployment
+fi
