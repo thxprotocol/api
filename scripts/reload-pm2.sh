b46d116cafef9703f494a3825ce6ee75131875f2
Peter Polman <peter@peterpolman.nl>
2021-01-23 13:51:03 +0100

    Changes output deployment script

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -4,5 +4,5 @@ if [ "$DEPLOYMENT_GROUP_NAME" == "develop" ]; then
 elif [ "$DEPLOYMENT_GROUP_NAME" == "master" ]; then
   pm2 kill && pm2 start ecosystem.config.js --env production --update-env
 else
-  echo "WARNING: Deployment failed.";
+  echo "WARNING: PM2 reload script failed."
 fi
