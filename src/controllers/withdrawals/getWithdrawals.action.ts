c23b91b4b95d1c509487dbf616a39947a4e3f93e
Peter Polman <peter@peterpolman.nl>
2021-03-17 09:11:41 +0100

    Update dosc

--- src/controllers/withdrawals/getWithdrawals.action.ts
+++ src/controllers/withdrawals/getWithdrawals.action.ts
@@ -8,7 +8,7 @@ import { HttpError, HttpRequest } from '../../models/Error';
  *   get:
  *     tags:
  *       - Withdrawals
- *     description: Get a list of withdrawals for the asset pool.
+ *     description: Get a list of withdrawals for a member of the asset pool.
  *     produces:
  *       - application/json
  *     parameters:
@@ -18,56 +18,55 @@ import { HttpError, HttpRequest } from '../../models/Error';
  *         type: string
  *     responses:
  *       '200':
- *          description: OK
- *          schema:
- *              withdrawPolls:
- *                  type: array
- *                  items:
- *                      type: object
- *                      properties:
- *                        id:
- *                            type: string
- *                            description: ID of the withdrawal.
- *                        beneficiary:
- *                            type: string
- *                            description: Beneficiary of the reward.
- *                        amount:
- *                            type: string
- *                            description: Rewarded amount for the beneficiary
- *                        approved:
- *                            type: string
- *                            description: Boolean reflecting the approved state of the withdrawal.
- *                        state:
- *                            type: number
- *                            description: WithdrawState [Pending, Withdrawn]
- *                        poll:
- *                            type: object
- *                            properties:
- *                               startTime:
- *                                  type: number
- *                                  description: Timestamp for the start time of the poll.
- *                               endTime:
- *                                  type: number
- *                                  description: Timestamp for the end time of the poll.
- *                               yesCounter:
- *                                  type: number
- *                                  description: Amount of yes votes for the poll.
- *                               noCounter:
- *                                  type: number
- *                                  description: Amount of no votes for the poll.
- *                               totalVoted:
- *                                  type: number
- *                                  description: Total amount of votes for the poll.
+ *         description: OK
+ *         schema:
+ *            type: array
+ *            items:
+ *              type: object
+ *              properties:
+ *                id:
+ *                    type: string
+ *                    description: ID of the withdrawal.
+ *                beneficiary:
+ *                    type: string
+ *                    description: Beneficiary of the reward.
+ *                amount:
+ *                    type: string
+ *                    description: Rewarded amount for the beneficiary
+ *                approved:
+ *                    type: string
+ *                    description: Boolean reflecting the approved state of the withdrawal.
+ *                state:
+ *                    type: number
+ *                    description: WithdrawState [Pending, Withdrawn]
+ *                poll:
+ *                    type: object
+ *                    properties:
+ *                       startTime:
+ *                          type: number
+ *                          description: Timestamp for the start time of the poll.
+ *                       endTime:
+ *                          type: number
+ *                          description: Timestamp for the end time of the poll.
+ *                       yesCounter:
+ *                          type: number
+ *                          description: Amount of yes votes for the poll.
+ *                       noCounter:
+ *                          type: number
+ *                          description: Amount of no votes for the poll.
+ *                       totalVoted:
+ *                          type: number
+ *                          description: Total amount of votes for the poll.
  *       '400':
- *          description: Bad Request. Indicates incorrect body parameters.
+ *         description: Bad Request. Indicates incorrect body parameters.
  *       '401':
- *          description: Unauthorized. Authenticate your request please.
+ *         description: Unauthorized. Authenticate your request please.
  *       '403':
- *          description: Forbidden. Your account does not have access to this pool.
+ *         description: Forbidden. Your account does not have access to this pool.
  *       '500':
- *          description: Internal Server Error.
+ *         description: Internal Server Error.
  *       '502':
- *          description: Bad Gateway. Received an invalid response from the network or database.
+ *         description: Bad Gateway. Received an invalid response from the network or database.
  */
 export const getWithdrawals = async (req: HttpRequest, res: Response, next: NextFunction) => {
     try {
