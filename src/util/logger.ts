51baf1b497977882d4192f09b462d7a732a236ed
Peter Polman <peter@peterpolman.nl>
2021-05-11 12:09:46 +0200

    Adds daily rotate to logs

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,6 +1,7 @@
 import morgan from 'morgan';
 import json from 'morgan-json';
 import winston from 'winston';
+import 'winston-daily-rotate-file';
 import { Request } from 'express';
 import { ENVIRONMENT, VERSION } from './secrets';
 
@@ -15,8 +16,21 @@ const instance = winston.createLogger({
     level: 'info',
     format: formatWinston,
     transports: [
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'logs/combined.log' }),
+        new winston.transports.DailyRotateFile({
+            filename: 'logs/error/%DATE%.log',
+            datePattern: 'YYYY-MM-DD-HH',
+            zippedArchive: true,
+            maxSize: '5m',
+            maxFiles: '30d',
+            level: 'error',
+        }),
+        new winston.transports.DailyRotateFile({
+            filename: 'logs/combined/%DATE%.log',
+            datePattern: 'YYYY-MM-DD-HH',
+            zippedArchive: true,
+            maxSize: '5m',
+            maxFiles: '30d',
+        }),
     ],
 });
 
@@ -25,23 +39,23 @@ if (ENVIRONMENT !== 'production' && ENVIRONMENT !== 'development') {
     instance.debug('Logging initialized at debug level');
 }
 
-morgan.token('timestamp', function getTimestamp() {
-    return new Date().toISOString().replace('T', ' ').substr(0, 19);
-});
+// morgan.token('timestamp', function getTimestamp() {
+//     return new Date().toISOString().replace('T', ' ').substr(0, 19);
+// });
 
 const formatMorgan = json({
     'method': ':method',
     'url': ':url',
     'status': ':status',
-    'timestamp': ':timestamp',
     'ip': ':remote-addr',
     'bytes': ':res[content-length]',
     'ms': ':response-time',
-    'user': 'remote-user',
+    'user': ':remote-user',
     'user-agent': ':user-agent',
 });
 
 export const logger = instance;
 export const requestLogger = morgan(formatMorgan, {
     skip: (req: Request) => req.baseUrl && req.baseUrl.startsWith(`/${VERSION}/ping`),
+    stream: { write: (message: string) => instance.info(message) },
 });
