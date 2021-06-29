6b820ed359efe7404641dde58950db75757a7e1b
Peter Polman <peter@peterpolman.nl>
2021-06-29 13:07:27 +0200

    Revert "Switch to WSS for default provider"
    This reverts commit 997641805f3bf02bad8c22da429e5ab82a95a501.

--- src/util/network.ts
+++ src/util/network.ts
@@ -34,9 +34,9 @@ export const FactoryArtifact = AssetPoolFactoryArtifact;
 export const getProvider = (npid: NetworkProvider) => {
     switch (npid) {
         case NetworkProvider.Test:
-            return new Web3(new Web3.providers.WebsocketProvider(TESTNET_RPC_WSS));
+            return new Web3(TESTNET_RPC);
         case NetworkProvider.Main:
-            return new Web3(new Web3.providers.WebsocketProvider(RPC_WSS));
+            return new Web3(RPC);
     }
 };
 
