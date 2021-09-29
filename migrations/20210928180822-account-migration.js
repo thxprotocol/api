b8e451420282c25612842e7e18051d551a98fe1c
Peter Polman <peter@peterpolman.nl>
2021-09-29 11:06:34 +0200

    Provided code formatting and added migrate-mongo up command to startup script

--- migrations/20210928180822-account-migration.js
+++ migrations/20210928180822-account-migration.js
@@ -1,8 +1,8 @@
 module.exports = {
-  async up(db) {
-    return db.collection('accounts').updateMany({}, { $set: { recoveryPhrase: 'Test phrase' } })
-  },
-  async down(db) {
-    return db.collection('accounts').updateMany({}, { $unset: { recoveryPhrase: null } })
-  }
+    async up(db) {
+        return db.collection('accounts').updateMany({}, { $set: { recoveryPhrase: 'Test phrase' } });
+    },
+    async down(db) {
+        return db.collection('accounts').updateMany({}, { $unset: { recoveryPhrase: null } });
+    },
 };
