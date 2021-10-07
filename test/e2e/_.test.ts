0eb07dbf7ca653fa0ef7b3b7e45e39367c207331
Peter Polman <peter@peterpolman.nl>
2021-10-07 13:03:50 +0200

    Improves ci compose overrides

--- test/e2e/_.test.ts
+++ test/e2e/_.test.ts
@@ -1,24 +1,12 @@
 import mongoose from 'mongoose';
 import db from '../../src/util/database';
 import MongoAdapter from '../../src/oidc/adapter';
-// import { MongoMemoryServer } from 'mongodb-memory-server';
 import { deployFacets } from '../../scripts/lib/facets';
 import { deployFactory } from '../../scripts/lib/factory';
 import { deployRegistry } from '../../scripts/lib/registry';
 import { NetworkProvider } from '../../src/util/network';
 
 beforeAll(async () => {
-    // const memServer = new MongoMemoryServer({
-    //     instance: {
-    //         ip: 'node',
-    //         port: 27027,
-    //         dbName: 'test',
-    //     },
-    //     autoStart: true,
-    // });
-
-    // await memServer.ensureInstance();
-
     console.log('Facets: ', await deployFacets(NetworkProvider.Test));
     console.log('Factory: ', await deployFactory(NetworkProvider.Test));
     console.log('Registry: ', await deployRegistry(NetworkProvider.Test));
@@ -32,12 +20,12 @@ afterAll(async () => {
 });
 
 // require('./encrypt.ts');
-// require('./api.ts');
+require('./api.ts');
 require('./signup.ts');
-// require('./bypass_polls.ts');
-// require('./propose_withdrawal.ts');
-// require('./unlimited_token.ts');
-// require('./voting.ts');
-// require('./roles.ts');
-// require('./gas_station.ts');
-// require('./oidc_admin.ts');
+require('./bypass_polls.ts');
+require('./propose_withdrawal.ts');
+require('./unlimited_token.ts');
+require('./voting.ts');
+require('./roles.ts');
+require('./gas_station.ts');
+require('./oidc_admin.ts');
