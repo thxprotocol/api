bdd2dc24c0d3e722afa7f30aa9c0293667abc946
Justina <justinamary27@gmail.com>
2021-10-11 13:18:06 +0530

    removed the check for rewardId

--- src/services/WithdrawalService.ts
+++ src/services/WithdrawalService.ts
@@ -64,7 +64,7 @@ export default class WithdrawalService {
             beneficiary,
             approved,
             state: WithdrawalState.Pending,
-            ...(rewardId > 0 ? { rewardId } : {}),
+            ...(rewardId ? { rewardId } : {}),
             poll: {
                 startTime,
                 endTime,
