4627a88e5017da731ba572ba982a83675deaad73
Justina <justinamary27@gmail.com>
2021-10-18 12:44:49 +0530

    provided detailed description on swagger auth

--- src/swaggerDef.js
+++ src/swaggerDef.js
@@ -8,17 +8,17 @@ module.exports = {
     apis: ['src/controllers/**/*.ts'],
     basePath: '/v1',
     securityDefinitions: {
-        "bearerAuth": {
-            "description": "Value: Bearer {jwt}",
-            "type": "apiKey",
-            "name": "Authorization",
-            "scheme": "bearer",
-            "in": "header"
-        }
+        bearerAuth: {
+            description: "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+            type: 'apiKey',
+            name: 'Authorization',
+            scheme: 'bearer',
+            in: 'header',
+        },
     },
     security: [
         {
-            "bearerAuth": []
-        }
+            bearerAuth: [],
+        },
     ],
 };
