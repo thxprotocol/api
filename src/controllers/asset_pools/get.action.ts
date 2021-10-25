826b92ebfe3a745e070ea0e2c768488ead2654b1
Justina <justinamary27@gmail.com>
2021-10-25 19:02:35 +0530

    rolled back the logic to get assetpool from request

--- src/controllers/asset_pools/get.action.ts
+++ src/controllers/asset_pools/get.action.ts
@@ -4,27 +4,31 @@ import AssetPoolService from '../../services/AssetPoolService';
 
 export const getAssetPool = async (req: HttpRequest, res: Response, next: NextFunction) => {
     try {
-        // const { assetPool, error } = await AssetPoolService.getByAddress(req.params.address);
-        const assetPool = req.assetPool;
-        const { token, error } = await AssetPoolService.getPoolToken(req.assetPool);
+        const { assetPool, error } = await AssetPoolService.getByAddress(req.params.address);
 
         if (error) {
-            throw new Error(error);
-        }
+            return next(new HttpError(500, 'Could not get this asset pool.'));
+        } else {
+            const { token, error } = await AssetPoolService.getPoolToken(req.assetPool);
 
-        res.json({
-            address: req.params.address,
-            ...{
-                sub: assetPool.sub,
-                rat: assetPool.rat,
-                address: assetPool.address,
-                network: assetPool.network,
-                bypassPolls: assetPool.bypassPolls,
-                proposeWithdrawPollDuration: assetPool.proposeWithdrawPollDuration,
-                rewardPollDuration: assetPool.rewardPollDuration,
-            },
-            token,
-        });
+            if (error) {
+                throw new Error(error);
+            }
+
+            res.json({
+                address: req.params.address,
+                ...{
+                    sub: assetPool.sub,
+                    rat: assetPool.rat,
+                    address: assetPool.address,
+                    network: assetPool.network,
+                    bypassPolls: assetPool.bypassPolls,
+                    proposeWithdrawPollDuration: assetPool.proposeWithdrawPollDuration,
+                    rewardPollDuration: assetPool.rewardPollDuration,
+                },
+                token,
+            });
+        }
     } catch (error) {
         return next(new HttpError(500, 'Could not obtain Asset Pool data from the network.', error));
     }
