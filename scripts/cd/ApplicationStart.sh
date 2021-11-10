e86fe71c1baa61434e580d415703c950275f7125
Peter Polman <peter@peterpolman.nl>
2021-11-10 17:03:07 +0100

    Remove bug from start script

--- scripts/cd/ApplicationStart.sh
+++ scripts/cd/ApplicationStart.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pm2 startOrReload cd /home/ubuntu/api/ecosystem.config.js --env production --update-env
+pm2 startOrReload /home/ubuntu/api/ecosystem.config.js --env production --update-env
