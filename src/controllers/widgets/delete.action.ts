c021660f14dea0b523e9651547bb5a04266149f2
Justina <justinamary27@gmail.com>
2021-11-11 17:04:09 +0530

    added delete controller for widget

---
+++ src/controllers/widgets/delete.action.ts
@@ -0,0 +1,22 @@
+import { Response, NextFunction } from 'express';
+import { HttpError, HttpRequest } from '../../models/Error';
+import WidgetService from '../../services/WidgetService';
+import ClientService from '../../services/ClientService';
+
+export const deleteWidget = async (req: HttpRequest, res: Response, next: NextFunction) => {
+    try {
+        const { client } = await ClientService.get(req.params.clientId);
+
+        if (!client) {
+            return next(new HttpError(500, 'Could not find a client for this clientId.'));
+        }
+
+        const { result } = await WidgetService.remove(req.params.clientId);
+
+        if (result) {
+            res.status(204).end();
+        }
+    } catch (e) {
+        next(new HttpError(500, 'Could not remove a widget for this clientId.', e));
+    }
+};
