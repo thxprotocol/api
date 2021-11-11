34bf53a51a2de90b6d6a51df3ae2542def596d0f
Justina <justinamary27@gmail.com>
2021-11-11 21:10:38 +0530

    added logic to remove the record from client

--- src/controllers/widgets/delete.action.ts
+++ src/controllers/widgets/delete.action.ts
@@ -8,12 +8,14 @@ export const deleteWidget = async (req: HttpRequest, res: Response, next: NextFu
         const { client } = await ClientService.get(req.params.clientId);
 
         if (!client) {
-            return next(new HttpError(500, 'Could not find a client for this clientId.'));
+            return next(new HttpError(500, 'Could not find a widget for this clientId.'));
         }
-
-        const { result } = await WidgetService.remove(req.params.clientId);
-
-        if (result) {
+        const { error } = await WidgetService.remove(req.params.clientId);
+        if (error) {
+            throw new Error(error);
+        } else {
+            await ClientService.remove(req.params.clientId);
+            
             res.status(204).end();
         }
     } catch (e) {
