029b3542f06d4506444fc4355e50e42be0be276c
Bram Rongen <bram@thx.network>
2022-02-07 00:23:08 +0100

    Improve processWithdrawals job
    - Stop the double bookkeeping of failReasons in jobs and withdrawals
    - Keep processing withdrawals on other failures then max fee
    - Start agenda in agenda file
    - Immediately trigger job in test for ~10 sec speed improvement

--- test/e2e/transaction_queue.ts
+++ test/e2e/transaction_queue.ts
@@ -15,8 +15,8 @@ import { Account } from 'web3-core';
 import { getToken } from './lib/jwt';
 import { createWallet, voter } from './lib/network';
 import { mockClear, mockStart, mockUrl } from './lib/mock';
-import { agenda } from '../../src/util/agenda';
-import WithdrawalService from '../../src/services/WithdrawalService';
+import { agenda, eventNameProcessWithdrawals } from '../../src/util/agenda';
+import { ERROR_MAX_FEE_PER_GAS } from '../../src/util/network';
 
 const user = request.agent(server);
 
@@ -64,12 +64,11 @@ describe('Transaction Queue', () => {
                 .expect(201);
         });
 
-        it('HTTP 302 when member is added', async () => {
-            await user
-                .post('/v1/members/')
+        it('HTTP 302 when member is added', (done) => {
+            user.post('/v1/members/')
                 .send({ address: userWallet.address })
                 .set({ AssetPool: poolAddress, Authorization: adminAccessToken })
-                .expect(302);
+                .expect(302, done);
         });
     });
 
@@ -77,7 +76,7 @@ describe('Transaction Queue', () => {
         let withdrawalDocumentIdList: any = [];
 
         it('should disable job processor', async () => {
-            await agenda.disable({ name: 'processWithdrawals' });
+            await agenda.disable({ name: eventNameProcessWithdrawals });
         });
 
         it('should propose 5 withdrawals', async () => {
@@ -141,45 +140,39 @@ describe('Transaction Queue', () => {
         });
 
         it('should enable job processor', async () => {
-            await agenda.enable({ name: 'processWithdrawals' });
+            await agenda.enable({ name: eventNameProcessWithdrawals });
         });
 
         it('should cast a fail event', (done) => {
             const callback = async (error: Error, job: Job) => {
-                agenda.off('fail:processWithdrawals', callback);
-
-                const { withdrawal } = await WithdrawalService.getById(job.attrs.data.documentId);
-
-                expect(withdrawal).toBeDefined();
-                expect(withdrawal.withdrawalId).toBeUndefined();
-                expect(withdrawal.failReason).toBeUndefined();
-
+                agenda.off(`fail:${eventNameProcessWithdrawals}`, callback);
+                expect(error.message).toBe(ERROR_MAX_FEE_PER_GAS);
                 done();
             };
-            agenda.on('fail:processWithdrawals', callback);
+            agenda.on(`fail:${eventNameProcessWithdrawals}`, callback);
+            agenda.now(eventNameProcessWithdrawals, null);
         });
 
         it('should remove exceeding gas price mock', async () => {
             mockClear();
             mockStart();
-            mockUrl('get', 'https://gasstation-mainnet.matic.network', '/v2', 200, feeData);
         });
 
         it('should cast a success event', (done) => {
             const callback = () => {
-                agenda.off('success:processWithdrawals', callback);
+                agenda.off(`success:${eventNameProcessWithdrawals}`, callback);
                 done();
             };
-            agenda.on('success:processWithdrawals', callback);
+            agenda.on(`success:${eventNameProcessWithdrawals}`, callback);
+            agenda.now(eventNameProcessWithdrawals, null);
         });
     });
 
     describe('POST /withdrawals 1x (failReason)', () => {
-        let withdrawalDocumentId = '',
-            failReason = '';
+        let withdrawalDocumentId = '';
 
         it('should disable job processor', async () => {
-            await agenda.disable({ name: 'processWithdrawals' });
+            await agenda.disable({ name: eventNameProcessWithdrawals });
         });
 
         it('should propose 1 withdrawal for unknown member', async () => {
@@ -200,19 +193,18 @@ describe('Transaction Queue', () => {
         });
 
         it('should enable job processor', async () => {
-            await agenda.enable({ name: 'processWithdrawals' });
+            await agenda.enable({ name: eventNameProcessWithdrawals });
         });
 
-        it('should cast a fail event', (done) => {
-            const callback = async (error: Error, job: Job) => {
-                failReason = job.attrs.failReason;
-                const documentId = job.attrs.data.documentId;
-                expect(documentId).toEqual(withdrawalDocumentId);
-                agenda.off('fail:processWithdrawals', callback);
+        // Even though the witdrawal fails the job doesn't. This allows other transactions to
+        // be processed immediately instead of during the following job execution.
+        it('should cast a success event', (done) => {
+            const callback = async () => {
+                agenda.off(`success:${eventNameProcessWithdrawals}`, callback);
                 done();
             };
-
-            agenda.on('fail:processWithdrawals', callback);
+            agenda.on(`success:${eventNameProcessWithdrawals}`, callback);
+            agenda.now(eventNameProcessWithdrawals, null);
         });
 
         it('should see a withdrawal with failReason', async () => {
@@ -221,7 +213,7 @@ describe('Transaction Queue', () => {
                 .set({ AssetPool: poolAddress, Authorization: walletAccessToken })
                 .expect(({ body }: Response) => {
                     expect(body.id).toEqual(withdrawalDocumentId);
-                    expect(body.failReason).toEqual(failReason);
+                    expect(body.failReason).not.toBe('');
                 })
                 .expect(200);
         });
