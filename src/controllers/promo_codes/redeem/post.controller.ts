04306f60443cccaec5427503be7ce32cab6cfc4f
Peter Polman <peter@peterpolman.nl>
2022-02-13 20:46:39 +0100

    Adds service method for redeemTokens

--- src/controllers/promo_codes/redeem/post.controller.ts
+++ src/controllers/promo_codes/redeem/post.controller.ts
@@ -1,10 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import { param } from 'express-validator';
-import { toWei } from 'web3-utils';
-import { callFunction, getProvider, sendTransaction, tokenContract } from '../../../util/network';
-import { getPromoCodeById } from '../../../services/PromoCodeService';
+import { callFunction, getProvider } from '../../../util/network';
+import { getPromoCodeById, redeemTokens } from '../../../services/PromoCodeService';
 import AccountProxy from '../../../proxies/AccountProxy';
-import { AmountExceedsAllowanceError, InsufficientBalanceError } from '../../../util/errors';
 
 const redeemPromoCodeValidation = [param('id').isString().isLength({ min: 23, max: 25 })];
 
@@ -12,31 +10,11 @@ async function RedeemPromoCodeController(req: Request, res: Response, next: Next
     const { admin } = getProvider(req.assetPool.network);
     const promoCode = await getPromoCodeById(req.params.id);
     const tokenAddress = await callFunction(req.assetPool.solution.methods.getToken(), req.assetPool.network);
-    const token = tokenContract(req.assetPool.network, tokenAddress);
     const { account } = await AccountProxy.getById(req.user.sub);
-    const amount = toWei(String(promoCode.price));
-    const allowance = await callFunction(
-        token.methods.allowance(account.address, admin.address),
-        req.assetPool.network,
-    );
 
-    if (allowance < amount) {
-        throw new AmountExceedsAllowanceError();
-    }
+    await redeemTokens(tokenAddress, account.address, admin.address, promoCode.price, req.assetPool.network);
 
-    const balance = await callFunction(token.methods.balanceOf(account.address), req.assetPool.network);
-
-    if (balance < amount) {
-        throw new InsufficientBalanceError();
-    }
-
-    const tx = await sendTransaction(
-        req.assetPool.address,
-        token.methods.transferFrom(account.address, admin.address, amount),
-        req.assetPool.network,
-    );
-
-    res.json(tx);
+    res.end(200);
 }
 
 export { redeemPromoCodeValidation };
--- src/services/PromoCodeService.ts
+++ src/services/PromoCodeService.ts
@@ -1,7 +1,9 @@
+import { toWei } from 'web3-utils';
 import { PromoCode } from '../models/PromoCode';
 import { IPromoCodeData } from '../interfaces/IPromoCodeData';
 import { paginatedResults } from '../util/pagination';
-import { PromoCodeNotFoundError } from '../util/errors';
+import { AmountExceedsAllowanceError, InsufficientBalanceError, PromoCodeNotFoundError } from '../util/errors';
+import { callFunction, NetworkProvider, sendTransaction, tokenContract } from '../util/network';
 
 async function createPromoCode(data: IPromoCodeData) {
     return await PromoCode.create(data);
@@ -22,4 +24,29 @@ async function listPromoCodesForSub(sub: string, page = 1, limit = 10) {
     return await paginatedResults(PromoCode, page, limit, { sub });
 }
 
-export { createPromoCode, getPromoCodeById, listPromoCodesForSub };
+async function redeemTokens(
+    tokenAddress: string,
+    owner: string,
+    spender: string,
+    price: number,
+    npid: NetworkProvider,
+) {
+    const token = tokenContract(npid, tokenAddress);
+
+    const amount = toWei(String(price));
+    const allowance = await callFunction(token.methods.allowance(owner, spender), npid);
+
+    if (allowance < amount) {
+        throw new AmountExceedsAllowanceError();
+    }
+
+    const balance = await callFunction(token.methods.balanceOf(owner), npid);
+
+    if (balance < amount) {
+        throw new InsufficientBalanceError();
+    }
+
+    return await sendTransaction(token.options.address, token.methods.transferFrom(owner, spender, amount), npid);
+}
+
+export { createPromoCode, getPromoCodeById, listPromoCodesForSub, redeemTokens };
