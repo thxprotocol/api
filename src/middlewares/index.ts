8a8ec965d76b409ad9f569ac6e0289d40756b60c
Bram Rongen <bram@thx.network>
2022-02-20 13:34:47 +0100

    Move error handling to middlewares folder

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,15 @@
 import { assertAssetPoolAccess } from './assertAssetPoolAccess';
 import { requireAssetPoolHeader } from './requireAssetPoolHeader';
 import { assertRequestInput } from './assertRequestInput';
+import { errorHandler } from './errorHandler';
+import { errorLogger } from './errorLogger';
+import { notFoundHandler } from './notFoundHandler';
 
-export { assertAssetPoolAccess, requireAssetPoolHeader, assertRequestInput };
+export {
+    assertAssetPoolAccess,
+    requireAssetPoolHeader,
+    assertRequestInput,
+    errorLogger,
+    errorHandler,
+    notFoundHandler,
+};
