b1b471bba7e4ef164cb9f4d7463660710c867d28
Bram Rongen <bram@thx.network>
2022-02-20 20:38:06 +0100

    Also change MailService

--- src/services/MailService.ts
+++ src/services/MailService.ts
@@ -10,33 +10,27 @@ const DURATION_DAY = Date.now() + 24 * 60 * 60 * 1000;
 
 export default class MailService {
     static async sendLoginLinkEmail(email: string, password: string) {
-        try {
-            const secureKey = encryptString(password, SECURE_KEY.split(',')[0]);
-            const authenticationToken = encryptString(createRandomToken(), password);
-            const html = await ejs.renderFile(
-                path.dirname(__dirname) + '/views/mail/loginLink.ejs',
-                {
-                    authenticationToken,
-                    secureKey,
-                    returnUrl: WALLET_URL,
-                    baseUrl: API_URL,
-                },
-                { async: true },
-            );
-            const { account, error } = await AccountProxy.getByEmail(email);
-
-            if (error) throw new Error(error);
-
-            await sendMail(email, 'A sign in is requested for your Web Wallet', html);
-
-            await AccountProxy.update(account.id, {
+        const secureKey = encryptString(password, SECURE_KEY.split(',')[0]);
+        const authenticationToken = encryptString(createRandomToken(), password);
+        const html = await ejs.renderFile(
+            path.dirname(__dirname) + '/views/mail/loginLink.ejs',
+            {
                 authenticationToken,
-                authenticationTokenExpires: DURATION_DAY,
-            });
+                secureKey,
+                returnUrl: WALLET_URL,
+                baseUrl: API_URL,
+            },
+            { async: true },
+        );
+        const { account, error } = await AccountProxy.getByEmail(email);
 
-            return { result: true };
-        } catch (error) {
-            return { error };
-        }
+        if (error) throw new Error(error);
+
+        await sendMail(email, 'A sign in is requested for your Web Wallet', html);
+
+        await AccountProxy.update(account.id, {
+            authenticationToken,
+            authenticationTokenExpires: DURATION_DAY,
+        });
     }
 }
