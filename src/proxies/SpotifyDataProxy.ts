3b5ea39da4898ab2ec9a5a02eda5bef4c65963fc
garfdev.13@gmail.com <garfdev.13@gmail.com>
2022-02-21 19:51:48 +0700

    feat: hook up with new error handler

--- src/proxies/SpotifyDataProxy.ts
+++ src/proxies/SpotifyDataProxy.ts
@@ -4,21 +4,17 @@ const NO_USER = 'Could not find spotify data for this account';
 
 export default class SpotifyDataProxy {
     static async getSpotify(sub: string) {
-        try {
-            const r = await authClient({
-                method: 'GET',
-                url: `/account/${sub}/spotify`,
-                headers: {
-                    Authorization: await getAuthAccessToken(),
-                },
-            });
+        const r = await authClient({
+            method: 'GET',
+            url: `/account/${sub}/spotify`,
+            headers: {
+                Authorization: await getAuthAccessToken(),
+            },
+        });
 
-            if (r.status !== 200) throw new Error(NO_USER);
-            if (!r.data) throw new Error(NO_USER);
+        if (r.status !== 200) throw new Error(NO_USER);
+        if (!r.data) throw new Error(NO_USER);
 
-            return { ...r.data };
-        } catch (error) {
-            return { error };
-        }
+        return r.data;
     }
 }
