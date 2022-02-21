bd60d49dd327ab963c27317ddcbf1790fea8589d
Bram Rongen <bram@thx.network>
2022-02-21 09:25:41 +0100

    Remove try/catch from gas_station/post.controller.ts

--- src/controllers/gas_station/post.controller.ts
+++ src/controllers/gas_station/post.controller.ts
@@ -1,6 +1,5 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { sendTransaction } from '@/util/network';
-import { HttpError } from '@/models/Error';
 import { hex2a, parseLogs, findEvent } from '@/util/events';
 import { Artifacts } from '@/util/artifacts';
 import { eventIndexer } from '@/util/indexer';
@@ -17,42 +16,38 @@ const eventNames = [
 
 export const createCallValidation = [body('call').exists(), body('nonce').exists(), body('sig').exists()];
 
-export const postCall = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const tx = await sendTransaction(
-            req.assetPool.solution.options.address,
-            req.assetPool.solution.methods.call(req.body.call, req.body.nonce, req.body.sig),
-            req.assetPool.network,
-            250000,
-        );
-        const events = parseLogs(Artifacts.IDefaultDiamond.abi, tx.logs);
-        const event = findEvent('Result', events);
+export const postCall = async (req: Request, res: Response) => {
+    const tx = await sendTransaction(
+        req.assetPool.solution.options.address,
+        req.assetPool.solution.methods.call(req.body.call, req.body.nonce, req.body.sig),
+        req.assetPool.network,
+        250000,
+    );
+    const events = parseLogs(Artifacts.IDefaultDiamond.abi, tx.logs);
+    const event = findEvent('Result', events);
 
-        if (event) {
-            if (!event.args.success) {
-                const error = hex2a(event.args.data.substr(10));
+    if (event) {
+        if (!event.args.success) {
+            const error = hex2a(event.args.data.substr(10));
 
-                return res.status(500).json({
-                    error,
-                });
-            }
+            return res.status(500).json({
+                error,
+            });
+        }
 
-            for (const eventName of eventNames) {
-                const event = findEvent(eventName, events);
+        for (const eventName of eventNames) {
+            const event = findEvent(eventName, events);
 
-                if (event) {
-                    const callback = indexer[`on${eventName}`];
+            if (event) {
+                const callback = indexer[`on${eventName}`];
 
-                    if (callback) {
-                        await callback(req.assetPool.network, req.assetPool.solution.options.address, event.args);
-                    }
+                if (callback) {
+                    await callback(req.assetPool.network, req.assetPool.solution.options.address, event.args);
                 }
             }
-
-            res.status(200).end();
         }
-    } catch (err) {
-        return next(new HttpError(502, 'gas_station/call failed.', err));
+
+        res.status(200).end();
     }
 };
 
