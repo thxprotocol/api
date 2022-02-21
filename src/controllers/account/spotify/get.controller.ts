8cba86135ec2ef3b6d75b2427ccf21879f15bf89
garfdev.13@gmail.com <garfdev.13@gmail.com>
2022-02-21 20:01:13 +0700

    feat: upgrade controller to new format

--- src/controllers/account/spotify/get.controller.ts
+++ src/controllers/account/spotify/get.controller.ts
@@ -1,25 +1,20 @@
-import { Request, Response, NextFunction } from 'express';
-import SpotifyDataProxy from '../../../proxies/SpotifyDataProxy';
-import { HttpError } from '../../../models/Error';
+import { Request, Response } from 'express';
+import SpotifyDataProxy from '@/proxies/SpotifyDataProxy';
 
-export const getSpotify = async (req: Request, res: Response, next: NextFunction) => {
+export const getSpotify = async (req: Request, res: Response) => {
     async function getSpotifyData() {
         const { isAuthorized, error, ...rest } = await SpotifyDataProxy.getSpotify(req.user.sub);
         if (error) throw new Error(error.message);
         return { isAuthorized, ...rest };
     }
 
-    try {
-        const { isAuthorized, users, ...rest } = await getSpotifyData();
+    const { isAuthorized, users, ...rest } = await getSpotifyData();
 
-        if (!isAuthorized) return res.json({ isAuthorized });
+    if (!isAuthorized) return res.json({ isAuthorized });
 
-        res.send({
-            users,
-            isAuthorized,
-            ...rest,
-        });
-    } catch (error) {
-        next(new HttpError(502, error.message, error));
-    }
+    res.send({
+        users,
+        isAuthorized,
+        ...rest,
+    });
 };
