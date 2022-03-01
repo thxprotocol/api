f61b9c937ffb3ef2dd07cde859a18fdf11ed9c0d
Peter Polman <peter@peterpolman.nl>
2022-03-01 15:41:59 +0100

    Reenable test

--- src/util/agenda.ts
+++ src/util/agenda.ts
@@ -4,11 +4,11 @@ import db from './database';
 
 import { jobProcessWithdrawals } from '@/jobs/processWithdrawals';
 import { jobRequireDeposits } from '@/jobs/requireDeposit';
-// import { jobRequireWithdraws } from '@/jobs/requireWithdraw';
+import { jobRequireWithdraws } from '@/jobs/requireWithdraw';
 
 export const eventNameProcessWithdrawals = 'processWithdrawals';
 export const eventNameRequireDeposits = 'requireDeposits';
-// export const eventNameRequireWithdraws = 'requireWithdraws';
+export const eventNameRequireWithdraws = 'requireWithdraws';
 
 export const agenda = new Agenda({
     maxConcurrency: 1,
@@ -18,7 +18,7 @@ export const agenda = new Agenda({
 
 agenda.define(eventNameProcessWithdrawals, jobProcessWithdrawals);
 agenda.define(eventNameRequireDeposits, jobRequireDeposits);
-// agenda.define(eventNameRequireWithdraws, jobRequireWithdraws);
+agenda.define(eventNameRequireWithdraws, jobRequireWithdraws);
 
 db.connection.once('open', async () => {
     agenda.mongo(db.connection.getClient().db(), 'jobs');
@@ -26,7 +26,7 @@ db.connection.once('open', async () => {
 
     agenda.every('5 seconds', eventNameProcessWithdrawals);
     agenda.every('5 seconds', eventNameRequireDeposits);
-    // agenda.every('5 seconds', eventNameRequireWithdraws);
+    agenda.every('5 seconds', eventNameRequireWithdraws);
 
     logger.info('Started agenda processing');
 });
