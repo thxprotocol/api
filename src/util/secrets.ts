a9ed44ea54b11016bc2e61e05a31877d38101ce4
Bram Rongen <bram@thx.network>
2022-03-02 09:38:47 +0100

    Remove unused env vars

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -8,7 +8,6 @@ const required = [
     'AUTH_URL',
     'API_URL',
     'WALLET_URL',
-    'PUBLIC_URL',
     'DASHBOARD_URL',
     'RPC',
     'ASSET_POOL_FACTORY_ADDRESS',
@@ -51,7 +50,6 @@ export const SECURE_KEY = process.env.SECURE_KEY;
 export const AUTH_URL = process.env.AUTH_URL;
 export const API_URL = process.env.API_URL;
 export const WALLET_URL = process.env.WALLET_URL;
-export const PUBLIC_URL = process.env.PUBLIC_URL;
 export const DASHBOARD_URL = process.env.DASHBOARD_URL;
 export const WIDGETS_URL = process.env.WIDGETS_URL;
 export const RPC = process.env.RPC;
@@ -66,15 +64,11 @@ export const COLLECTOR = process.env.COLLECTOR;
 export const PORT = process.env.PORT;
 export const AUTH_CLIENT_ID = process.env.AUTH_CLIENT_ID;
 export const AUTH_CLIENT_SECRET = process.env.AUTH_CLIENT_SECRET;
-export const GTM = process.env.GTM;
 export const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 export const RATE_LIMIT_REWARD_GIVE = Number(process.env.RATE_LIMIT_REWARD_GIVE);
 export const RATE_LIMIT_REWARD_CLAIM = Number(process.env.RATE_LIMIT_REWARD_CLAIM);
 export const RATE_LIMIT_REWARD_GIVE_WINDOW = Number(process.env.RATE_LIMIT_REWARD_GIVE_WINDOW);
 export const RATE_LIMIT_REWARD_CLAIM_WINDOW = Number(process.env.RATE_LIMIT_REWARD_CLAIM_WINDOW);
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-export const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
 export const INITIAL_ACCESS_TOKEN = process.env.INITIAL_ACCESS_TOKEN;
 export const CIRCULATING_SUPPLY = process.env.CIRCULATING_SUPPLY;
 export const MAX_FEE_PER_GAS = String(process.env.MAX_FEE_PER_GAS);
