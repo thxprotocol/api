fa27fbd9846c0bfc1be86661680fe9949c1ebebe
GarfDev <garfdev.13@gmail.com>
2022-03-23 19:01:27 +0700

    feat: add getAllERC20Token

--- src/controllers/token/_.routing.ts
+++ src/controllers/token/_.routing.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 
-import { getCirculatingSupply } from './getToken.action';
+import checkScopes from 'express-jwt-authz';
 
 import erc20Route from './erc20/_.routing';
+import { getCirculatingSupply } from './getToken.action';
 
 const router = express.Router();
 
 router.get('/cs', getCirculatingSupply);
-router.use('/erc20', erc20Route);
+router.use('/erc20', checkScopes(['dashboard']), erc20Route);
 
 export default router;
