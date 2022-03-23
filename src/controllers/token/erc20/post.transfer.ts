5124b34dbbfe5e205bc3e45680949c517c5b144e
GarfDev <garfdev.13@gmail.com>
2022-03-23 20:57:33 +0700

    feat: add get_by_id, get_all route and add transferAllERC20MintedToken method

--- src/controllers/token/erc20/post.transfer.ts
+++ src/controllers/token/erc20/post.transfer.ts
@@ -1,4 +1,8 @@
 import { Request, Response } from 'express';
+<<<<<<< HEAD
 import ERC20Service from '@/services/ERC20Service';
+=======
+import TokenService from '@/services/TokenService';
+>>>>>>> 2054b1e (feat: add get_by_id, get_all route and add transferAllERC20MintedToken method)
 
 const transferToken = async (req: Request, res: Response) => {};
