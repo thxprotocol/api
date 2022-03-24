473ce3f59ff2dcd36d757bf3d801245567be3121
GarfDev <garfdev.13@gmail.com>
2022-03-24 19:44:18 +0700

    feat: move Token model create to inside service

--- src/controllers/token/erc20/post.create.action.ts
+++ src/controllers/token/erc20/post.create.action.ts
@@ -1,23 +1,13 @@
 import { Request, Response } from 'express';
 
-import Token from '@/models/Token';
 import TokenService from '@/services/TokenService';
 
 export const postCreateToken = async (req: Request, res: Response) => {
-    const { token, receipt } = await TokenService.createERC20Token({
+    const { token } = await TokenService.createERC20Token({
         name: req.body['name'],
         symbol: req.body['symbol'],
         network: req.body['network'],
         totalSupply: req.body['totalSupply'],
-    });
-
-    await Token.create({
-        name: req.body['name'],
-        symbol: req.body['symbol'],
-        network: req.body['network'],
-        totalSupply: req.body['totalSupply'],
-        blockNumber: receipt.blockNumber,
-        transactionHash: receipt.transactionHash,
         sub: req.user.sub,
     });
 
--- src/services/TokenService.test.ts
+++ src/services/TokenService.test.ts
@@ -1,20 +1,62 @@
-import { NetworkProvider } from '@/types/enums';
+import BN from 'bn.js';
 import { Contract } from 'web3-eth-contract';
+import { fromWei } from 'web3-utils';
+
+import { MONGODB_URI } from '@/config/secrets';
+import { NetworkProvider } from '@/types/enums';
+import { getProvider } from '@/util/network';
+import db from '@/util/database';
+
 import TokenService from './TokenService';
 
 describe('TokenService', () => {
     const TOTAL_SUPPLY = 1000;
+    const DEFAULT_SUB = 'an-unbeliveable-belief-1122';
+
     let limitedToken: Contract;
+
+    beforeAll(async () => {
+        await db.connect(MONGODB_URI);
+    });
+
+    afterAll(async () => {
+        await db.truncate();
+    });
+
     it('Able to deploy limited token', async () => {
         const { token } = await TokenService.createERC20Token({
             name: 'Test Token',
             symbol: 'TTK',
             network: NetworkProvider.Test,
             totalSupply: `${TOTAL_SUPPLY}`,
+            sub: DEFAULT_SUB,
         });
 
         limitedToken = token;
         expect(limitedToken).toBeDefined();
     });
-    // it('Able to deploy unlimited token', async () => {});
+
+    it('Able to deploy unlimited token', async () => {
+        const { token } = await TokenService.createERC20Token({
+            name: 'Test Token',
+            symbol: 'TTK',
+            network: NetworkProvider.Test,
+            totalSupply: '0', // 0 = unlimited
+            sub: DEFAULT_SUB,
+        });
+
+        expect(token).toBeDefined();
+    });
+
+    it('Admin hold correct minted balance', async () => {
+        const { admin } = getProvider(NetworkProvider.Test);
+        const adminBalance: BN = await limitedToken.methods.balanceOf(admin.address).call();
+
+        expect(Number(fromWei(adminBalance))).toEqual(TOTAL_SUPPLY);
+    });
+
+    it('Able to query all deployed tokens by a user', async () => {
+        const tokens = await TokenService.getAllERC20TokenBySub(DEFAULT_SUB);
+        expect(tokens.length).toEqual(2);
+    });
 });
