a4a090d906bb199e9ab6612212bc157d23df6386
GarfDev <garfdev.13@gmail.com>
2022-03-24 19:54:19 +0700

    chore: rename TokenService and it methods to ERC20

--- src/controllers/token/erc20/get.action.ts
+++ src/controllers/token/erc20/get.action.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
-import TokenService from '@/services/TokenService';
+import ERC20Service from '@/services/ERC20Service';
 
-export const getERC20TokenById = async (req: Request, res: Response) => {
+export const getById = async (req: Request, res: Response) => {
     const id = req.params['id'];
-    const token = await TokenService.getERC20TokenById(id);
+    const token = await ERC20Service.getById(id);
     return res.send({ token: token.toJSON() });
 };
--- src/controllers/token/erc20/get.all.action.ts
+++ src/controllers/token/erc20/get.all.action.ts
@@ -1,7 +1,7 @@
-import TokenService from '@/services/TokenService';
+import ERC20Service from '@/services/ERC20Service';
 import { Request, Response } from 'express';
 
 export const getAllERC20Token = async (req: Request, res: Response) => {
-    const tokens = await TokenService.getAllERC20TokenBySub(req.user.sub);
+    const tokens = await ERC20Service.getAll(req.user.sub);
     return res.send({ tokens: tokens.map((token) => token.toJSON()) });
 };
