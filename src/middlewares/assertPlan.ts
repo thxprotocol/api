a2c7c57676d8d7ba9eaea2c94c016118c67ff55a
Peter Polman <peter@peterpolman.nl>
2022-03-28 15:24:03 +0200

    Adds plantype middleware

---
+++ src/middlewares/assertPlan.ts
@@ -0,0 +1,14 @@
+import AccountProxy from '@/proxies/AccountProxy';
+import { AccountPlanType, NetworkProvider } from '@/types/enums';
+import { ForbiddenError } from '@/util/errors';
+import { NextFunction } from 'express';
+
+export function assertPlan(plans: AccountPlanType[]) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const account = await AccountProxy.getById(req.assetPool.sub);
+        if (plans.includes(account.plan) && req.assetPool.network === NetworkProvider.Main) {
+            throw new ForbiddenError('Active plan of this pool owner is not sufficient.');
+        }
+        next();
+    };
+}
---
+++ src/types/enums/AccountPlanType.ts
@@ -0,0 +1,6 @@
+export enum AccountPlanType {
+    None = -1,
+    Solo = 0,
+    Community = 1,
+    Creator = 2,
+}
