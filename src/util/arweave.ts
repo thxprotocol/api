945afca05ca793ae440e901c8fc8e76b3d7f36fc
GarfDev <garfdev.13@gmail.com>
2022-06-30 21:24:19 +0700

    feat: support static temp key

--- src/util/arweave.ts
+++ src/util/arweave.ts
@@ -1,5 +1,7 @@
 import Arweave from 'arweave';
 
+let TEMP_KEY: any = null;
+
 const TEST_CONFIG = { host: '127.0.0.1', port: 1984, protocol: 'http' };
 const PROD_CONFIG = {
     host: 'arweave.net',
@@ -14,7 +16,10 @@ console.log(process.env.NODE_ENV, IS_TESTING ? TEST_CONFIG : PROD_CONFIG);
 const arweave = Arweave.init(IS_TESTING ? TEST_CONFIG : PROD_CONFIG);
 
 export const getArweaveKey = async () => {
-    if (IS_TESTING) return await arweave.wallets.generate();
+    if (IS_TESTING) {
+        if (!TEMP_KEY) TEMP_KEY = await arweave.wallets.generate();
+        return TEMP_KEY;
+    }
     return JSON.parse(process.env.ARWEAVE_KEY);
 };
 
