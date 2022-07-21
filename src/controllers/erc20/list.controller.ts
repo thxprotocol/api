c3c5079f4e3611710785c02347292ad1519ae604
valeriagrazzini <vgrazzini@gmail.com>
2022-07-21 09:31:42 +0200

    modified controllers list logic

--- src/controllers/erc20/list.controller.ts
+++ src/controllers/erc20/list.controller.ts
@@ -1,3 +1,4 @@
+import ERC20 from '@/models/ERC20';
 import ERC20Service from '@/services/ERC20Service';
 import { Request, Response } from 'express';
 import { query } from 'express-validator';
@@ -6,9 +7,14 @@ export const validation = [query('archived').optional().isBoolean()];
 
 export const controller = async (req: Request, res: Response) => {
     // #swagger.tags = ['ERC20']
-    const erc20s = await ERC20Service.getAll(req.auth.sub);
-    const archived = req.query.archived == 'true' ? true : false;
-    return res.send(erc20s.filter((x) => x.archived === archived).map(({ _id }) => _id));
+    let erc20s;
+    if (req.query.archived == 'true') {
+        erc20s = await ERC20Service.getAll(req.auth.sub);
+    } else {
+        erc20s = await ERC20.find({ sub: req.auth.sub, archived: false });
+    }
+
+    return res.send(erc20s.map(({ _id }) => _id));
 };
 
 export default { controller, validation };
--- src/controllers/pools/list.controller.ts
+++ src/controllers/pools/list.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { AssetPoolDocument } from '@/models/AssetPool';
+import { AssetPool, AssetPoolDocument } from '@/models/AssetPool';
 import AssetPoolService from '@/services/AssetPoolService';
 import { query } from 'express-validator';
 
@@ -7,9 +7,13 @@ export const validation = [query('archived').optional().isBoolean()];
 
 const controller = async (req: Request, res: Response) => {
     // #swagger.tags = ['Pools']
-    const pools = await AssetPoolService.getAllBySub(req.auth.sub);
-    const archived = req.query.archived == 'true' ? true : false;
-    const list = pools.filter((x) => x.archived === archived).map((pool: AssetPoolDocument) => pool._id);
+    let pools;
+    if (req.query.archived == 'true') {
+        pools = await AssetPoolService.getAllBySub(req.auth.sub);
+    } else {
+        pools = await AssetPool.find({ sub: req.auth.sub, archived: false });
+    }
+    const list = pools.map((pool: AssetPoolDocument) => pool._id);
 
     res.json(list);
 };
