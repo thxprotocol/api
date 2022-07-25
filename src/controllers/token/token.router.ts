47af85876a0b7aea01cf9ae7b4e4e97ad2717847
Peter <peter@thx.network>
2022-07-25 14:07:01 +0200

    Adds token total supply endpoint

--- src/controllers/token/token.router.ts
+++ src/controllers/token/token.router.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
-import ReadToken from './get.controller';
+import ReadTokenCirculatingSupply from './cs/get.controller';
+import ReadTokenTotalSupply from './ts/get.controller';
 
 const router = express.Router();
 
-router.get('/cs', ReadToken.controller);
+router.get('/cs', ReadTokenCirculatingSupply.controller);
+router.get('/ts', ReadTokenTotalSupply.controller);
 
 export default router;
