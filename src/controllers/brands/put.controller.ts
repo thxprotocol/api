dcfc3685f65c1fdccdc066832720aea4cadf0d4b
Peter <peter@thx.network>
2022-07-25 17:11:35 +0200

    Remove console.log

--- src/controllers/brands/put.controller.ts
+++ src/controllers/brands/put.controller.ts
@@ -6,7 +6,6 @@ import BrandService from '../../services/BrandService';
 export default {
     validation: [body('logoImgUrl').optional().isURL(), body('backgroundImgUrl').optional().isURL()],
     controller: async (req: Request, res: Response) => {
-        console.log(req.assetPool._id);
         const brand = await BrandService.update({ poolId: req.assetPool._id }, req.body);
         res.json(brand);
     },
