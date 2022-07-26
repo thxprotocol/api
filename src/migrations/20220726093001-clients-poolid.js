5094f56ddf8bee050c96ea37f7d3a59993d56d0a
Peter <peter@thx.network>
2022-07-26 15:55:55 +0200

    Make clients migration forgiving

--- src/migrations/20220726093001-clients-poolid.js
+++ src/migrations/20220726093001-clients-poolid.js
@@ -5,19 +5,23 @@ module.exports = {
         const widgetsColl = db.collection('widget');
         const clients = await clientsColl.find().toArray();
         const promises = clients.map(async (client) => {
-            const pool = await poolsColl.findOne({ clientId: client.clientId });
-            if (pool) {
-                return await clientsColl.updateOne(
-                    { _id: client._id },
-                    { $set: { poolId: String(pool._id), grantType: 'client_credentials' } },
-                );
-            }
-            const widget = await widgetsColl.findOne({ clientId: client.clientId });
-            if (widget) {
-                return await clientsColl.updateOne(
-                    { _id: client._id },
-                    { $set: { poolId: String(pool._id), grantType: 'authorization_code' } },
-                );
+            try {
+                const pool = await poolsColl.findOne({ clientId: client.clientId });
+                if (pool) {
+                    return await clientsColl.updateOne(
+                        { _id: client._id },
+                        { $set: { poolId: String(pool._id), grantType: 'client_credentials' } },
+                    );
+                }
+                const widget = await widgetsColl.findOne({ clientId: client.clientId });
+                if (widget) {
+                    return await clientsColl.updateOne(
+                        { _id: client._id },
+                        { $set: { poolId: String(pool._id), grantType: 'authorization_code' } },
+                    );
+                }
+            } catch (error) {
+                console.error(error);
             }
         });
         await Promise.all(promises);
