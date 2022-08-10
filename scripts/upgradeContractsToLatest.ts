149c3def7242c0695bee63fea08fefc6426e1ebc
Peter <peter@thx.network>
2022-08-10 13:14:07 +0200

    Extends upgradeContracts script with ownership transfer and setpoolregistry

--- scripts/upgradeContractsToLatest.ts
+++ scripts/upgradeContractsToLatest.ts
@@ -1,14 +1,67 @@
 import db from '@/util/database';
+import AssetPoolService from '@/services/AssetPoolService';
+import AccountProxy from '@/proxies/AccountProxy';
+import Web3 from 'web3';
 import { MONGODB_URI } from '@/config/secrets';
 import { getContract } from '@/config/contracts';
 import { updateDiamondContract } from '@/util/upgrades';
 import { AssetPool } from '@/models/AssetPool';
 import { AccountPlanType, ChainId } from '@/types/enums';
 import { ContractName, currentVersion, DiamondVariant } from '@thxnetwork/artifacts';
-import AssetPoolService from '@/services/AssetPoolService';
-import AccountProxy from '@/proxies/AccountProxy';
+import { HARDHAT_RPC, POLYGON_MUMBAI_RPC, POLYGON_RPC, PRIVATE_KEY } from '@/config/secrets';
+import { getDiamondAbi } from '@/config/contracts';
+import { getProvider } from '@/util/network';
+import { logger } from '@/util/logger';
+import { toChecksumAddress } from 'web3-utils';
 
 db.connect(MONGODB_URI);
+
+const networks: any = {};
+if (HARDHAT_RPC) {
+    networks[ChainId.Hardhat] = (() => {
+        const web3 = new Web3(HARDHAT_RPC);
+        return { web3, admin: web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY) };
+    })();
+}
+
+if (POLYGON_MUMBAI_RPC) {
+    networks[ChainId.PolygonMumbai] = (() => {
+        const web3 = new Web3(POLYGON_MUMBAI_RPC);
+        return { web3, admin: web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY) };
+    })();
+}
+
+if (POLYGON_RPC) {
+    networks[ChainId.Polygon] = (() => {
+        const web3 = new Web3(POLYGON_RPC);
+        return { web3, admin: web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY) };
+    })();
+}
+
+const send = async (web3: Web3, to: string, fn: any) => {
+    const [from] = await web3.eth.getAccounts();
+    const data = fn.encodeABI();
+    const gas = await fn.estimateGas({ from });
+    const gasPrice = await web3.eth.getGasPrice();
+    const nonce = await web3.eth.getTransactionCount(from, 'pending');
+    const sig = await web3.eth.accounts.signTransaction(
+        {
+            gas,
+            to,
+            from,
+            data,
+            nonce,
+            gasPrice,
+        },
+        PRIVATE_KEY,
+    );
+    try {
+        const receipt = await web3.eth.sendSignedTransaction(sig.rawTransaction);
+        logger.debug(receipt.transactionHash);
+    } catch (error) {
+        logger.debug(error);
+    }
+};
 
 async function main() {
     const startTime = Date.now();
@@ -39,8 +92,35 @@ async function main() {
             const isFreeMumbai = account.plan === AccountPlanType.Free && pool.chainId === ChainId.PolygonMumbai;
 
             if (isPaidPlan || isFreeMumbai) {
-                console.log('Upgrade:', pool.address, `${pool.variant} ${pool.version} -> ${currentVersion}`);
+                // create provider for chain with PRIVATE_KEY
+                const oldProvider = networks[pool.chainId];
+                // get new provider for chain with getProvider()
+                const newProvider = getProvider(pool.chainId);
+                const { methods } = new oldProvider.web3.eth.Contract(
+                    getDiamondAbi(pool.chainId, 'defaultDiamond'),
+                    pool.address,
+                );
+
+                const currentOwner = toChecksumAddress(await methods.owner().call());
+                const newOwner = toChecksumAddress(newProvider.defaultAccount);
+
+                if (currentOwner !== newOwner) {
+                    logger.debug('TransferOwnership:', pool.address, `${currentOwner} -> ${newOwner}`);
+                    await send(oldProvider.web3, pool.address, methods.transferOwnership(newOwner));
+                }
+
+                logger.debug('Upgrade:', pool.address, `${pool.variant} ${pool.version} -> ${currentVersion}`);
                 await AssetPoolService.updateAssetPool(pool, currentVersion);
+
+                const currentRegistryAddress = toChecksumAddress(await pool.contract.methods.getRegistry().call());
+                const registryAddress = toChecksumAddress(
+                    getContract(pool.chainId, 'Registry', currentVersion).options.address,
+                );
+
+                if (registryAddress !== currentRegistryAddress) {
+                    logger.debug('SetRegistry:', pool.address, `${currentRegistryAddress} -> ${registryAddress}`);
+                    await send(newProvider.web3, pool.address, methods.setRegistry(registryAddress));
+                }
             }
         } catch (error) {
             console.error(pool.address, error);
