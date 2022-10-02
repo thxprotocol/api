9b86f55f5025f27f43e43e11c3816182eb56c421
Bram Rongen <bram@thx.network>
2022-10-02 12:19:17 +0200

    Remove commented out check receipts agenda job

--- src/util/agenda.ts
+++ src/util/agenda.ts
@@ -2,22 +2,19 @@ import db from './database';
 import { Agenda } from 'agenda';
 import { logger } from './logger';
 import { updatePendingTransactions } from '@/jobs/updatePendingTransactions';
-// import { checkReceipts } from '@/jobs/checkReceipts';
 import { generateRewardQRCodesJob } from '@/jobs/rewardQRcodesJob';
 
 const agenda = new Agenda({
     name: 'jobs',
     maxConcurrency: 1,
     lockLimit: 1,
-    processEvery: '1 seconds',
+    processEvery: '1 second',
 });
 
 const EVENT_UPDATE_PENDING_TRANSACTIONS = 'updatePendingTransactions';
-// const EVENT_UPDATE_PENDING_TRANSACTIONS_RECEIPTS = 'updatePendingTransactionReceipts';
 const EVENT_SEND_DOWNLOAD_QR_EMAIL = 'sendDownloadQrEmail';
 
 agenda.define(EVENT_UPDATE_PENDING_TRANSACTIONS, updatePendingTransactions);
-// agenda.define(EVENT_UPDATE_PENDING_TRANSACTIONS_RECEIPTS, checkReceipts);
 agenda.define(EVENT_SEND_DOWNLOAD_QR_EMAIL, generateRewardQRCodesJob);
 
 db.connection.once('open', async () => {
@@ -26,7 +23,6 @@ db.connection.once('open', async () => {
     await agenda.start();
 
     agenda.every('30 seconds', EVENT_UPDATE_PENDING_TRANSACTIONS);
-    // agenda.every('1 seconds', EVENT_UPDATE_PENDING_TRANSACTIONS_BC);
     agenda.every('5 seconds', EVENT_SEND_DOWNLOAD_QR_EMAIL);
 
     logger.info('AgendaJS successfully started job processor');
