978e40b63e6b97e64d2d08166f90d8a807750d54
Peter <peter@thx.network>
2022-10-03 22:32:26 +0200

    Implements morgan-body

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,12 @@ import compression from 'compression';
 import express from 'express';
 import lusca from 'lusca';
 import path from 'path';
+import morganBody from 'morgan-body';
 
-import { MONGODB_URI, PORT, VERSION } from '@/config/secrets';
+import { MONGODB_URI, NODE_ENV, PORT, VERSION } from '@/config/secrets';
 import router from '@/controllers';
 import { corsHandler, errorLogger, errorNormalizer, errorOutput, notFoundHandler } from '@/middlewares';
 import db from '@/util/database';
-import { requestLogger } from '@/util/logger';
 
 axiosBetterStacktrace(axios);
 
@@ -22,12 +22,16 @@ db.connect(MONGODB_URI);
 
 app.set('trust proxy', true);
 app.set('port', PORT);
-app.use(requestLogger);
 app.use(compression());
 app.use(lusca.xframe('SAMEORIGIN'));
 app.use(lusca.xssProtection(true));
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 app.use(express.json());
+
+if (!['test', 'production'].includes(NODE_ENV)) {
+    morganBody(app);
+}
+
 app.use(express.urlencoded({ extended: true }));
 app.use(corsHandler);
 app.use(`/${VERSION}`, router);
--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,8 +1,6 @@
 import morgan from 'morgan';
-import json from 'morgan-json';
 import winston from 'winston';
-import { Request } from 'express';
-import { NODE_ENV, VERSION } from '@/config/secrets';
+import { NODE_ENV } from '@/config/secrets';
 
 const formatWinston = winston.format.combine(
     winston.format.timestamp({
@@ -18,18 +16,6 @@ export const logger = winston.createLogger({
     transports: [new winston.transports.Console()],
 });
 
-const formatMorgan = json({
-    'method': ':method',
-    'url': ':url',
-    'status': ':status',
-    'ip': ':remote-addr',
-    'bytes': ':res[content-length]',
-    'ms': ':response-time',
-    'user': ':remote-user',
-    'user-agent': ':user-agent',
-});
-
-export const requestLogger = morgan(formatMorgan, {
-    skip: (req: Request) => req.baseUrl && req.baseUrl.startsWith(`/${VERSION}/ping`),
+export const requestLogger = morgan('tiny', {
     stream: { write: (message: string) => logger.http(JSON.parse(message)) },
 });
