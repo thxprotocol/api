b6eeac844e08e07bcd412a7deb0bd0dd8c0bd84f
valeriagrazzini <vgrazzini@gmail.com>
2022-10-04 12:02:46 +0200

    fixed canvas size and centered logo

--- src/services/ImageService.ts
+++ src/services/ImageService.ts
@@ -1,8 +1,8 @@
 import { createCanvas, loadImage } from 'canvas';
 import QRCode from 'qrcode';
 import short from 'short-uuid';
-import { AWS_S3_PUBLIC_BUCKET_NAME, AWS_S3_PUBLIC_BUCKET_REGION } from '@/config/secrets';
-import { s3Client } from '@/util/s3';
+import { AWS_S3_PUBLIC_BUCKET_NAME, AWS_S3_PUBLIC_BUCKET_REGION } from '../config/secrets';
+import { s3Client } from '../util/s3';
 import { GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
@@ -30,9 +30,9 @@ export default {
         return `https://${AWS_S3_PUBLIC_BUCKET_NAME}.s3.${AWS_S3_PUBLIC_BUCKET_REGION}.amazonaws.com/${key}`;
     },
     createQRCode: async (url: string, logo: Buffer | string) => {
-        const width = 55;
-        const center = 75;
-        const canvas = createCanvas(width, width);
+        const canvasSize = 220;
+        const imgSize = 58;
+        const canvas = createCanvas(canvasSize, canvasSize);
 
         await QRCode.toCanvas(canvas, url, {
             errorCorrectionLevel: 'H',
@@ -41,11 +41,17 @@ export default {
                 dark: '#000000',
                 light: '#ffffff',
             },
+            width: canvasSize,
         });
 
         const ctx = canvas.getContext('2d');
+
         const img = await loadImage(logo);
-        ctx.drawImage(img, center, center, width, width);
+
+        const positionX = ctx.canvas.height / 2 - imgSize / 2;
+        const positionY = ctx.canvas.width / 2 - imgSize / 2;
+
+        ctx.drawImage(img, positionX, positionY, imgSize, imgSize);
 
         const qrCode = canvas.toDataURL('image/png');
         return qrCode.replace(/^data:image\/png;base64,/, '');
